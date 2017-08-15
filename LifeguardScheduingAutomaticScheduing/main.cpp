14998359d0a3267e1f8cb927ace6c786cfebabc6
fuookami <bagawolf2014@yahoo.co.jp>
2017-08-15 08:25:11 +0800

    构建项目

--- LifeguardScheduingAutomaticScheduing/main.cpp
+++ LifeguardScheduingAutomaticScheduing/main.cpp
@@ -1,10 +1,8 @@
-#include "LifeguardScheduingAutomaticScheduing.h"
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	LifeguardScheduingAutomaticScheduing w;
-	w.show();
+
 	return a.exec();
 }
